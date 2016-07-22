70b542f3c6f8239a6b68b7fd92691838afe563e6
Megaju <julien_malle@outlook.fr>
2016-07-22 17:19:34 +0200

    nouvelle étape, tjs pas bon

--- js/script.js
+++ js/script.js
@@ -13,35 +13,39 @@
 // minutes - secondes - milisecondes
 var m = 0;
 var s = 0;
-// ça c'est juste histoire d'avoir un pomodoro bien stressant !
-var ms = 0
 
 // state chrono
-var state = false;
+var state = true;
+var demandePause = false;
 
 // mettre en route le chrono
-$("#play").click = (state = true);
+//$("#play").click = (state = true);
 // arrêt du chrono
-////$("#pause").click = (state = false);
+//$("#pause").click = (demandePause = true);
 
 // le décompte tourne
 $("#play").click(function () {
     if (state === true) {
-        // decompte secondes
-        setInterval(function () {
-            if(s == 59) {s = 0; m++; document.getElementById("minutes").innerHTML = m;}
-            s++;
-            document.getElementById("secondes").innerHTML = s;
-        }, 1000);
-        // decompte miliseconde pour les fous
-        setInterval(function () {
-            if (ms == 1000) {ms = 0; };
-            ms++;
-            document.getElementById("milisecondes").innerHTML = ms;
-        }, 10);
+        if (demandePause === false) {
+            // decompte secondes
+            setInterval(function () {
+                if (s == 60) {
+                    s = 0;
+                    m++;
+                    document.getElementById("minutes").innerHTML = m;
+                }
+                if (s >= 10) {
+                    document.getElementById("secondes").innerHTML = s;
+                } else {
+                    document.getElementById("secondes").innerHTML = "0" + s;
+                }
+                s++;
+            }, 100);
+        }
     } else {
-        //alert('pause');
-    };
+        // mettre en pause
+        state = false;
+    }
 });
 
 // le truc qui marche pas pck on est bloqué dans notre if plus haut...
@@ -49,10 +53,3 @@ if (s == 3) {
     m++;
     document.getElementById("minutes").innerHTML = m;
 };
-
-// mettre en pause (pour les noobs du pomodoro)
-// genre "haaaaa j'dois aller faire pipi en fait..."
-// INCAPABLE DE SE RETENIR !
-// Et du coup... y a du taf en plus à faire ! BRAVO !
-/*$("#pause").click(function ()) {
-    state = false;*/
